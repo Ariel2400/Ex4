b33dba22e0318ba164a529bf1d7e7a81d294b98c
Ariel Grosh <ariel241202@gmail.com>
2020-09-24 20:16:26 +0300

    extracted problem to a seperate module

--- src/algorithms/matrix_searcher.hpp
+++ src/algorithms/matrix_searcher.hpp
@@ -1,7 +1,12 @@
 #include <memory>
 #include <string>
-
+#pragma once
+#include "problem.hpp"
 #include "../matrices/matrix_class.hpp"
+#include "DFS_matrix_searcher.hpp"
+#include "BestFS_matrix_searcher.hpp"
+#include "BFS_matrix_searcher.hpp"
+#include "A-star_matrix_searcher.hpp"
 
 typedef enum {
   PATH_FOUND = 0,
@@ -9,19 +14,11 @@ typedef enum {
   OUT_OF_BOUNDS_INDEX = 2,
 } SearchStatus;
 
-typedef struct {
-  std::unique_ptr<Matrix> matrix;
-  int start_row;
-  int start_column;
-  int end_row;
-  int end_column;
-} Problem;
-
 class MatrixSearcher {
 public:
   /* problem contains a matrix and start / end coordinates.
-     assigns a description of a path from start to end into solution
+     assigns a description of a path from start to end uint32_to solution
      and its weight into weight. returns a fitting search status. */
   virtual SearchStatus search(const Problem &problem, std::string *solution,
-                              int *weight) = 0;
+                              uint32_t *weight) = 0;
 };
